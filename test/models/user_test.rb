47ddf69721078ddde720348a8c4949a8b8df8d5d
Guilherme Silva Paim <guisilvapaim@gmail.com>
2019-03-13 09:59:14 -0300

    Modify test fixtures

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "should not save user without email" do
+  test "should not save user" do
     user = User.new
     assert_not user.save
   end
